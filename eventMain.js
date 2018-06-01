659c9ec397ec7cbce480ec65226333b6119eb0f4
liuze <admin@liuze.org>
2018-06-01 20:51:10 +0800

    'hello-word'

--- eventMain.js
+++ eventMain.js
@@ -3,3 +3,13 @@ var a = "你好";
 var b = "Hello Word !!";
 
 console.info(a,b);
+
+var gitFn = function() {
+    console.log(123);
+}
+
+gitFn();
+
+setTimeout(function() {
+    console.info(2333);
+},1000)
